b682aefc925b0b5cca14537f0740c0f335aaa7a0
terranadams <terran.adams7225@stu.mtec.edu>
2020-12-22 08:46:46 -0700

    added store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import cartTotal from './reducers'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+const store = createStore(cartTotal)
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
