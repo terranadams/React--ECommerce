044f17a18e9e8aceff8b63eada65ed7d27ed1f81
terranadams <terran.adams7225@stu.mtec.edu>
2020-12-22 10:23:49 -0700

    working on filtering

--- src/Products.js
+++ src/Products.js
@@ -9,17 +9,35 @@ function Products() {
   }, [])
 
   const [items, setItems] = useState([])
+  const [categoryFilter, setCategoryFilter] = useState('')
   
   const fetchItems = async () => {
-    const data = await fetch('https://my-json-server.typicode.com/tdmichaelis/json-api/products')
+    const data = await fetch(`https://my-json-server.typicode.com/tdmichaelis/json-api/products`)
     const items = await data.json()
     setItems(items)
     console.log(items)
   }
   
+  // if (categoryFilter) {
+  //   let filteredItems = items.filter(item => {
+  //     item.category == categoryFilter
+  //   })
+  //   setItems(filteredItems)
+  // }
+  
   return (
     <div>
       <h1>Product Page</h1>
+      <select name="categories" onChange={e=>setCategoryFilter(e.target.value)}>
+        <option value="">All Items</option>
+        <option value="headphones">Headphones</option>
+        <option value="phone">Phone</option>
+        <option value="tv">Television</option>
+        <option value="small-appliance">Small Appliance</option>
+        <option value="refrigerator">Refrigerator</option>
+        <option value="watch">Watch</option>
+        <option value="action-camera">Action-Camera</option>
+      </select>
       {items.map(item => (
          <div key={item.id}>{item.title}
          <Link to={`products/${item.id}`}>
