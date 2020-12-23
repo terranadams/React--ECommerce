e4ac6751572cdc23f00fb57374bc41e34fee7193
terranadams <terran.adams7225@stu.mtec.edu>
2020-12-23 11:38:37 -0700

    kinda fixed itemsList reducer, not giving me errors now

--- src/reducers.js
+++ src/reducers.js
@@ -21,16 +21,18 @@ const itemsTotalPrice = (state = 0, action) => {
     }
 }
 const itemsList = (state = [], action) => {
-    console.log(state)
     switch(action.type){
         case 'ADD_ITEM':
-            return state.push(action.payload)
+            return [...state, action.payload]
         default:
             return state
     }
 }
-// return {   state.itemsList.concat(action.payload)   }
-// return {   [...state.itemsList, action.payload]  }
+// Immutability is what lets react-redux efficiently subscribe to fine-grained updates of your state. It also enables time-travel with redux-devtools
+// NOT return state.push(action.payload) because it mutates state which is wrong.
+// NOT return state.itemsList.concat(action.payload) because it mutates state which is wrong.
+
+// in itemList, "return [...state, action.payload]" seems to work but issue seems to come from handler now, it adds things to the store unexpectedly.
 
 
 const reducers = combineReducers({ // We're exporting this thing which holds all our reducer functions
