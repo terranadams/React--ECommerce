b65473937c05faa2eacd7ab43a2c571059e502b0
terranadams <terran.adams7225@stu.mtec.edu>
2020-12-22 19:41:24 -0700

    fixed filter

--- src/Products.js
+++ src/Products.js
@@ -5,31 +5,35 @@ import { Link } from 'react-router-dom'
 
 function Products() {
   useEffect(() => {
-    fetchItems() 
+    fetchItems()
   }, [])
 
   const [items, setItems] = useState([])
-  const [categoryFilter, setCategoryFilter] = useState('')
-  
+  const [currentItems, setCurrentItems] = useState([])
+
   const fetchItems = async () => {
     const data = await fetch(`https://my-json-server.typicode.com/tdmichaelis/json-api/products`)
     const items = await data.json()
     setItems(items)
+    setCurrentItems(items);
     console.log(items)
   }
-  
-  // if (categoryFilter) {
-  //   let filteredItems = items.filter(item => {
-  //     item.category == categoryFilter
-  //   })
-  //   setItems(filteredItems)
-  // }
-  
+
+  const onCategoryChange = (value) => { // This is the change handler for the filter
+    if (value === 'all') {
+            setCurrentItems(items)
+    } else {
+      let filteredItems = items.filter(item => item.category === value);
+    setCurrentItems(filteredItems)
+    }
+  }
+
+
   return (
     <div>
       <h1>Product Page</h1>
-      <select name="categories" onChange={e=>setCategoryFilter(e.target.value)}>
-        <option value="">All Items</option>
+      <select name="categories" onChange={e => onCategoryChange(e.target.value)}>
+        <option value="all">All Items</option>
         <option value="headphones">Headphones</option>
         <option value="phone">Phone</option>
         <option value="tv">Television</option>
@@ -38,15 +42,15 @@ function Products() {
         <option value="watch">Watch</option>
         <option value="action-camera">Action-Camera</option>
       </select>
-      {items.map(item => (
-         <div key={item.id}>{item.title}
-         <Link to={`products/${item.id}`}>
-         <Product src={item.img} alt={item.title}/>
-         </Link>
-         </div>
+      {currentItems.map(item => (
+        <div key={item.id}>{item.title}
+          <Link to={`products/${item.id}`}>
+            <Product src={item.img} alt={item.title} />
+          </Link>
+        </div>
       ))}
     </div>
-  ); 
+  );
 }
 
 
