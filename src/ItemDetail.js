c008ee3005400d146f0faad755ee43d7c0cd399e
terranadams <terran.adams7225@stu.mtec.edu>
2021-01-02 22:21:53 -0700

    worked on images

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -19,6 +19,7 @@ function ItemDetail({ match }) {
 
     setItem(item)
     console.log(item)
+    
   }
 
   const addItem = (item) => {
@@ -35,12 +36,41 @@ function ItemDetail({ match }) {
 
   const dispatch = useDispatch()// We must import the ability to dispatch info to the store.
 
+  let width = "200px"
+  let height = "200px"
+
+  switch(item.category) {
+    case 'tv':
+      width = '450px'
+      height = '300px'
+      break
+    case 'headphones':
+      width = '200px'
+      height = '350px'
+      break
+    case 'phone':
+      width = '200px'
+      height = '300px'
+      break
+    case 'action-camera':
+      width = '250px'
+      height = '200px'
+      break
+    case 'watch':
+      width = '200px'
+      height = '300px'
+      break
+    case 'refrigerator':
+      width = '200px'
+      height = '350px'
+  }
+  
 
   return (
     <div>
       <Link to="/products"><button className="backButton">Go Back</button></Link>
       <h1>{item.title}</h1>
-      <img src={item.img} alt={item.title} width="300" height="300" />
+      <img src={item.img} alt={item.title} width={width} height={height} />
       <p>{item.description}</p>
       <p>${item.price}</p>
       <button onClick={() => {//THIS (for whatever reason) has to be an arrow function...
