64eb184bf9e41a664eb04b528fba591f4283c6fb
terranadams <terran.adams7225@stu.mtec.edu>
2021-01-05 09:44:43 -0700

    reorganized a lil code

--- src/Cart.js
+++ src/Cart.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Product from './Product'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 
 function Cart() {
