93d56789102e2f2da6be140fbfebcd29857ae21c
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-01-30 16:38:55 -0700

    fixed footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,12 @@ import './App.css';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer style={{
+            position: 'absolute',
+            bottom: 0,
+            width: '100%',
+            height: '2.5rem'     /* Footer height */
+          }}>
             <p>Terran Adams All Rights Reserved or something whatever I'm supposed to put here...</p>
         </footer>
     )
